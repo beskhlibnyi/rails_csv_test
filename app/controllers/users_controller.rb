27d5cb8bb1e01b1b646b92b4485fbe95c29b39cb
beskhlebnyi <yaroslav@chimplie.com>
2019-09-13 18:27:13 +0300

    setup csv generating and downloading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv, filename: "users-#{Date.today}.csv" }
+    end
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
-class User < ApplicationRecord
+require 'csv'
+
+class User < ActiveRecord::Base
+
+  def self.to_csv
+    required_fields = %w(id first_name last_name city age gender)
+
+    CSV.generate do |csv|
+      csv << required_fields
+
+      all.each do |user|
+        csv << user.attributes.values_at(*required_fields)
+      end
+    end
+  end
 end
